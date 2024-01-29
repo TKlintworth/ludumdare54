01a237da7003cf2e03d9dff7d8ac0e124d900948
Tristan Klintworth <frostflag@gmail.com>
2024-01-28 22:21:48 -0500

    start damage numbers scene

---
+++ Scenes/damage_number_2d.gd
@@ -0,0 +1,24 @@
+class_name DamageNumber2D
+extends Node2D
+
+@onready var label:Label = %Label
+@onready var label_container:Node2D = %LabelContainer
+@onready var ap:AnimationPlayer = %AnimationPlayer
+
+func _ready():
+	pass
+	
+func set_values_and_animate(value:String, start_pos:Vector2, height:float, spread:float) -> void:
+	label.text = value
+	ap.play("new_animation")
+	
+	var tween = get_tree().create_tween()
+	var end_pos = Vector2(randf_range(-spread, spread), -height) + start_pos
+	var tween_length = ap.get_animation("new_animation").length
+	
+	tween.tween_property(label_container, "position", end_pos, tween_length).from(start_pos)
+
+func remove():
+	ap.stop()
+	if is_inside_tree():
+		get_parent().remove_child(self)
